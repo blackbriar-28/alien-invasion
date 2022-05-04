30d20d07281f35638c3725a41c678e4e3e769476
blackbriar-28 <highstuff10@gmail.com>
2022-05-04 00:49:47 -0500

    Adding images

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings:
     """ A class to store all settings for Alien Invasion """
 
-    def __init__(self) -> None:
+    def __init__(self):
         """Initialize the game's settings. """
 
         #Screen settings
