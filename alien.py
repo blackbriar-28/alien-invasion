028e0b44edb39649daa1f4bef8f1aa933a9c77a8
blackbriar-28 <highstuff10@gmail.com>
2022-07-20 09:26:05 -0500

    Adding funtioanlity so the alien fleet can move back and forth

--- alien.py
+++ alien.py
@@ -8,7 +8,7 @@ class Alien(Sprite):
 
         super().__init__()
         self.screen = ai_game.screen
-
+        self.settings = ai_game.settings
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
@@ -16,3 +16,14 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right or right"""
+        self.x += ( self.settings.alien_speed * self.settings.fleet_direction )
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
--- alien_invasion.py
+++ alien_invasion.py
@@ -48,12 +48,26 @@ class AlienInvasion:
             alien.rect.y = alien_height + 2 * alien.rect.height * row_number
             self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _update_bullets(self):
@@ -66,6 +80,12 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+    
+    def _update_aliens(self):
+        """Check if the fleet is at an edge,
+            then update position of all aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _check_events(self):
         """ Respond to keypresses and mouse event """
