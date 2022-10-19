d0c32d2446d858cc8344fe2240cf86369cdd3dd1
blackbriar-28 <highstuff10@gmail.com>
2022-10-18 20:48:55 -0500

    Adding game_stats class for handling statistics in game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,10 @@
 import sys
+from time import sleep
+
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -18,6 +21,10 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
+        # Create an instance to store game statistics
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -101,6 +108,29 @@ class AlienInvasion:
             then update position of all aliens in the fleet"""
         self._check_fleet_edges()
         self.aliens.update()
+
+        #Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            # The ship was hit
+            self._ship_hit()
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        
+        # Decrement ships left.
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaining aliens and bullets.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Pause
+        sleep(0.5)
+
 
     def _check_events(self):
         """ Respond to keypresses and mouse event """
--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ class Settings:
         self.screen_height = 700 #900
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         #Bullet settings
         self.bullet_speed = 1.5
